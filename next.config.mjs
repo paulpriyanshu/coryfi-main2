75ee57657eea329261de232ccab6d7c57f9c50b0
Priyanshu Paul <priyanshupaul@Priyanshus-Laptop.local>
2024-10-23 01:01:23 +0530

    added

--- next.config.mjs
+++ next.config.mjs
@@ -17,12 +17,7 @@ const nextConfig = {
   
     images:{
         domains:['example.com','prac2.priyanshu-paul003.workers.dev','drive.google.com','i.imgur.com','cdn.pixabay.com','images.unsplash.com','en.wikipedia.org','assets.aceternity.com'],
-        // remotePatterns: [
-        //     {
-        //       protocol: 'https',
-        //       hostname: '**',      // Allow all HTTPS images - adjust as needed
-        //     },
-        //   ],
+
     },
     // output:"export"
 };
