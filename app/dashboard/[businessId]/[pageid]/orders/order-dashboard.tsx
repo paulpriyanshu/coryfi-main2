a35e08029ae5e6d99978503930bce87440efef56
paulpriyanshu <priyanshu.paul003@gmail.com>
2025-07-28 23:03:31 +0530

    new_changes1

--- app/dashboard/[businessId]/[pageid]/orders/order-dashboard.tsx
+++ app/dashboard/[businessId]/[pageid]/orders/order-dashboard.tsx
@@ -4,7 +4,7 @@ import { Download, Search, ShoppingBag, Package, User, ChevronDown, ChevronUp, F
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/Input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Collapsible, CollapsibleContent } from "@/components/ui/collapsible"
