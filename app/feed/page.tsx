a4ce4b7d05193b85cc2cf47490a0ac4db8252710
paulpriyanshu <priyanshu.paul003@gmail.com>
2025-08-07 16:51:06 +0530

    added_partial_loading2

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -31,7 +31,7 @@ export default async function Page() {
         <div className="grid grid-cols-1 lg:grid-cols-7 gap-6">
           <div className="hidden lg:block lg:col-span-2">
             <div className="sticky top-4">
-              <LeftSidebar userEmail={session.user.email} />
+              <LeftSidebar userEmail={session?.user?.email} />
             </div>
           </div>
           <div className="lg:col-span-3 space-y-5 dark:bg-black">
